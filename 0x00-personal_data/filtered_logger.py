7ddffb762e7018a29cf7472fde2e2c020101f7aa
afecego <afecego@gmail.com>
2022-05-19 14:43:24 -0500

    Implement a get_logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,6 +1,32 @@
 #!/usr/bin/env python3
 """function called filter_datum that returns the log message obfuscated"""
 import re
+import logging
+
+
+PII_FIELDS = ('name', 'email', 'phone', 'sn', 'password')
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        """function constructor"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+
+    def format(self, record: logging.LogRecord) -> str:
+        """method to filter values in incoming record using filter_datum"""
+        resum = filter_datum(self.fields, self.REDACTION,
+                            super(RedactingFormatter, self).format(record),
+                            self.SEPARATOR)
+        return resum
 
 
 def filter_datum(fields, redaction, message, separator):
@@ -10,3 +36,15 @@ def filter_datum(fields, redaction, message, separator):
         message = re.sub(i + "=.*?" + separator, i + "=" + redaction +
                          separator, message)
     return(message)
+
+
+def get_logger() -> logging.Logger:
+    """function that takes no arguments and returns a logging.Logger object"""
+    logger = logging.getLogger('user_data')
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    ch.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(ch)
+    return logger
