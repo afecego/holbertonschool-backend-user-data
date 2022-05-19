1ed452bd8aeeaaf967f144a3a7b9edc263d47092
afecego <afecego@gmail.com>
2022-05-19 15:53:15 -0500

    correction type of arguments

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 """function called filter_datum that returns the log message obfuscated"""
+import logging
 import re
 from typing import List
+
+
+PII_FIELDS = ('name', 'email', 'phone', 'sn', 'password')
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
@@ -12,3 +16,34 @@ def filter_datum(fields: List[str], redaction: str, message: str,
         message = re.sub(i + "=.*?" + separator, i + "=" + redaction +
                          separator, message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """function constructor"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """method to filter values in incoming record using filter_datum"""
+        return filter_datum(self.fields, self.REDACTION,
+                            super(RedactingFormatter, self).format(record),
+                            self.SEPARATOR)
+
+
+def get_logger() -> logging.Logger:
+    """function that takes no arguments and returns a logging.Logger object"""
+    logger = logging.getLogger('user_data')
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    ch.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(ch)
+    return logger
