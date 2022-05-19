5ea0b311e86a7e1c9c84b648d9a84ecbc772252c
afecego <afecego@gmail.com>
2022-05-19 15:50:57 -0500

    correction number of lines

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 """function called filter_datum that returns the log message obfuscated"""
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator) -> str:
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
     """function should use a regex to replace occurrences of certain field
     values"""
-    new_mess = message
     for i in fields:
-        new_mess = re.sub(i + "=.*?" + separator, i + "=" + redaction +
-                          separator, new_mess)
-    return new_mess
+        message = re.sub(i + "=.*?" + separator, i + "=" + redaction +
+                         separator, message)
+    return message
