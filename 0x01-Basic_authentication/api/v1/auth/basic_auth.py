c178940b639ccdb7e4a1e19c595a8b8add61f227
afecego <afecego@gmail.com>
2022-05-24 18:17:52 -0500

    return tupla whit credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -32,3 +32,16 @@ class BasicAuth(Auth):
             return decod.decode('utf-8')
         except binascii.Error:
             return None
+
+    def extract_user_credentials(self, decoded_base64_authorization_header:
+                                 str) -> (str, str):
+        """returns the user email and password from the Base64 decoded value"""
+        if decoded_base64_authorization_header is None:
+            return (None, None)
+        if type(decoded_base64_authorization_header) is not str:
+            return (None, None)
+        if ':' in decoded_base64_authorization_header:
+            a = decoded_base64_authorization_header.split(':')
+            (b, c) = (a[0], a[1])
+            return (b, c)
+        return (None, None)
