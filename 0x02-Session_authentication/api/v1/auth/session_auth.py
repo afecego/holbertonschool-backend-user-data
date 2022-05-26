3732cebe2c68be6476b40869fbfd706087851ea4
afecego <afecego@gmail.com>
2022-05-25 23:20:26 -0500

    Get user id for session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -8,7 +8,6 @@ class SessionAuth(Auth):
     """inherits from Auth"""
     user_id_by_session_id = {}
 
-
     def create_session(self, user_id: str = None) -> str:
         """creates a Session ID for a user_id"""
         if user_id is None:
@@ -18,3 +17,11 @@ class SessionAuth(Auth):
         id = str(uuid.uuid4())
         self.user_id_by_session_id[id] = user_id
         return id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """returns a User ID based on a Session ID"""
+        if session_id is None:
+            return None
+        if type(session_id) is not str:
+            return None
+        return self.user_id_by_session_id.get(session_id)
