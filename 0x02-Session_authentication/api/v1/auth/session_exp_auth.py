1b47271baa9089001abbc09da18ea4c85d582e11
afecego <afecego@gmail.com>
2022-05-28 21:44:17 -0500

    expiration date to a Session ID

---
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""Create a class SessionExpAuth"""
+from datetime import datetime, timedelta
+import os
+from api.v1.auth.session_auth import SessionAuth
+
+
+class SessionExpAuth(SessionAuth):
+    """inherits from SessionAuth"""
+    def __init__(self):
+        """Overload method"""
+        self.session_duration = int(os.getenv('SESSION_DURATION'))
+        if not self.session_duration or type(self.session_duration) is not str:
+            self.session_duration = 0
+
+    def create_session(self, user_id=None):
+        """create session"""
+        try:
+            session = super().create_session(user_id)
+        except Exception:
+            return None
+
+        session_dictionary = {
+            'user_id': user_id,
+            'created_at': datetime.now()
+        }
+        self.user_id_by_session_id[session] = session_dictionary
+        return session
+
+    def user_id_for_session_id(self, session_id=None):
+        """return user_id from the session dictionary"""
+        if session_id is None:
+            return None
+        diction = self.user_id_by_session_id.get(session_id)
+        if diction is None:
+            return None
+        if self.session_duration <= 0:
+            return diction.get('user_id')
+        if 'created_at' is not diction:
+            return None
+
+        time = diction.get('created_at')
+        session_connect = timedelta(seconds=self.session_duration)
+
+        if time + session_connect < datetime.now():
+            return None
+        else:
+            return diction.get('user_id')
