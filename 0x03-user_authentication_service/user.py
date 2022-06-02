ab682826239a1cb42984fb1dc2c729877646112a
afecego <afecego@gmail.com>
2022-06-01 20:33:28 -0500

    correction syntaxis

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 """create a SQLAlchemy model named User"""
-from re import I
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, Integer
 
