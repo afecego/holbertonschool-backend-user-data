8a2b1510168fb5971fd0acfe6dd40a03dec2f377
afecego <afecego@gmail.com>
2022-06-02 18:47:46 -0500

    register user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -27,7 +27,7 @@ class Auth:
         User object."""
         try:
             self._db.find_user_by(email=email)
-            raise ValueError(f'User {email} already exists.')
+            raise ValueError(f'User {email} already exists')
         except NoResultFound:
             hash_pass = _hash_password(password)
             user = self._db.add_user(email, hash_pass)
