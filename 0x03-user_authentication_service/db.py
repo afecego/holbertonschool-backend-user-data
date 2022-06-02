7538ed4df1de1ca4be4faef57248cdc17cf38b59
afecego <afecego@gmail.com>
2022-06-02 18:19:19 -0500

    update user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -53,3 +53,14 @@ class DB:
         if filt is None:
             raise NoResultFound
         return filt
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """will use find_user_by to locate the user to update"""
+        user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if hasattr(user, key):
+                setattr(user, key, value)
+            else:
+                raise ValueError
+        self._session.commit()
+        return None
