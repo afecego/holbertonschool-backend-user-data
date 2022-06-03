c61f47d06dcb3318cf7916f00c6f45d0540b05eb
afecego <afecego@gmail.com>
2022-06-02 22:23:20 -0500

    Verification user whit POST

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 """set up a basic Flask app"""
-from flask import Flask, jsonify
+from re import A
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'])
@@ -11,6 +14,21 @@ def index():
     """return a JSON payload"""
     form = {"message": "Bienvenue"}
     return jsonify(form)
+
+
+@app.route('/users', methods=['POST'])
+def users():
+    """end-point to register a user"""
+    email = request.form['email']
+    password = request.form['password']
+
+    try:
+        if AUTH.register_user(email, password) is not None:
+            payload = {f"email": {email}, "message": "user created"}
+            return jsonify(payload)
+    except ValueError:
+        no_payload = {"message": "email already registered"}
+        return jsonify(no_payload), 400
 
 
 if __name__ == "__main__":
