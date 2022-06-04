f5e0c1f32c8764e36609b93266717aa5587ec877
afecego <afecego@gmail.com>
2022-06-04 00:05:29 -0500

    Update password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -91,3 +91,12 @@ class Auth:
             return token
         except NoResultFound:
             raise ValueError
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """takes reset_token string argument and a password string
+        argument and returns None"""
+        us = self._db.find_user_by(reset_token=reset_token)
+        if not us:
+            raise ValueError
+        hashset = _hash_password(password)
+        self._db.update_user(us.id, hashed_password=hashset, reset_token=None)
