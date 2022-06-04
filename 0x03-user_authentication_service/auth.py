56ba7614182d1c376058b2bf2508179fb6dfc2d6
afecego <afecego@gmail.com>
2022-06-03 21:38:34 -0500

    Find user by id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -62,9 +62,12 @@ class Auth:
         self._db.update_user(us.id, session_id=session_id)
         return session_id
 
-    def get_user_from_session_id(self, session_id: str) -> Union[str, None]:
+    def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
         """returns the corresponding User or None"""
-        if not session_id or session_id is None:
+        if session_id is None:
             return None
-        else:
-            return self._db.find_user_by(session_id=session_id)
+        try:
+            found = self._db.find_user_by(session_id=session_id)
+            return found
+        except NoResultFound:
+            return None
